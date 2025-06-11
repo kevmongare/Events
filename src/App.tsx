dc1a88f800fda5c0625e0f5a2481c31e2bc6f280
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-06-11 20:39:36 +0300

    hero section update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState, useEffect } from "react";
+
+import img1 from "/bg.jpeg";
+import img2 from "/bg.jpeg";
+import img3 from "/bg.jpeg";
+
+const slides = [
+  { image: img1, title: "Weddings", text: "Elegant and unforgettable." },
+  { image: img2, title: "Corporate Events", text: "Professional and polished." },
+  { image: img3, title: "Birthdays", text: "Celebrate with style." },
+];
 import './App.css'
 import { Link as ScrollLink } from 'react-scroll';
 
 function App() {
- 
+ const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prev) => (prev + 1) % slides.length);
+    }, 5000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const current = slides[index];
 
   return (
     <>
-       <header className="fixed top-0 left-0 w-full z-50 bg-white shadow-lg">
+    {/* menu bar */}
+       <section className="fixed top-0 left-0 w-full z-50 bg-white shadow-lg">
       <div className="w-full bg-pink-600 p-2">
         <div className="flex justify-between text-white max-w-7xl mx-auto">
           <div className="flex items-center space-x-4">
@@ -78,10 +96,30 @@ function App() {
         </ScrollLink>
           </li>
         </ul>
-
-        
       </nav>
-    </header>
+    </section>
+    {/* header section */}
+
+    <div className="relative w-full h-[80vh] mt-30 shadow-gray-700 overflow-hidden z-40 max-w-7xl mx-auto rounded-lg "> {/* Adjusted height */}
+      {/* Background image */}
+      <img
+  src={current.image}
+  alt={current.title}
+  className="absolute top-0 left-0 w-full  h-[80vh] object-cover z-10"
+/>
+
+<div className="absolute inset-0 bg-black/70 z-20 flex flex-col justify-center items-center text-center px-4">
+  <h2 className="text-white text-4xl md:text-6xl font-bold mb-4">
+    {current.title}
+  </h2>
+  <p className="text-white text-lg md:text-xl pb-10">{current.text}</p>
+
+       <a href="#contact"
+          className="hidden md:inline-block bg-pink-600 text-white px-5 py-2 rounded-full font-bold shadow-md hover:bg-pink-700 transition">
+          Book Event
+        </a>
+</div>
+    </div>
     </>
   )
 }
